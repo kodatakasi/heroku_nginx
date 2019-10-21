6a3bb84e0b8f76dff3365cab23a6c44fab15b1f8
koda takasi <kodatakasi@gmail.com>
2019-10-21 17:31:41 +0900

    graphql install

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+      mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    end
+  post "/graphql", to: "graphql#execute"
   root to: 'blogs#index'
   resources :blogs do
     collection do
       post :confirm
     end
   end
+
 end
