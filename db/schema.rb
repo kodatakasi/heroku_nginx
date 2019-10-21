74ad32deac35b6146b44551a358c38ec7880a20b
koda takasi <kodatakasi@gmail.com>
2019-10-21 12:16:18 +0900

    init

---
+++ db/schema.rb
@@ -0,0 +1,12 @@
+
+ActiveRecord::Schema.define(version: 2019_08_01_224052) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+  end
+
+end
